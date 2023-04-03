2c59fb4486a2519d08901d871c6c7ffbf36f7ad8
joaquin <joaquin@barberis.com>
2023-04-03 01:15:26 -0400

    search engine working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,44 @@
 //var api="https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=29ba1603f1abdb6eacfabacffe49d96d"
 var token = "29ba1603f1abdb6eacfabacffe49d96d";
 var city = "San Diego"
-var api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + token + "&units=imperial";
-console.log(api);
+var elName = document.querySelector("#cityName");
+var elTemp = document.querySelector("#temp");
+var elWind = document.querySelector("#wind")
+var elHumidity = document.querySelector("#humidity")
+function createUrl(city) {
+    return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + token + "&units=imperial";
+}
 
-function cityWeather(city) {
-    fetch(api).then(function (response) {
+function weatherFetch(city) { 
+    var apiUrl = createUrl(city);
+
+    fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
+                elName.textContent = data?.name;
+                elTemp.textContent = data?.main.temp;
+                elWind.textContent = data?.wind.speed;
+                elHumidity
+                
             })
         }
         
     })
+ }
+
+function submitHandler(event) {
+    event.preventDefault()
+    city = event.target.elements.city.value;
+  
+    weatherFetch(city);
+
 }
-cityWeather("Naples")
+
+weatherFetch(city);
+
+
+
+
+var elSearch= document.querySelector("#city");
+document.addEventListener("submit", submitHandler);
