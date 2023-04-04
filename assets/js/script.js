1654f1c3441d5d5c6c6972d419368eb7a47ef3b7
joaquin <joaquin@barberis.com>
2023-04-03 23:29:18 -0400

    forcast future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 //var api="https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=29ba1603f1abdb6eacfabacffe49d96d"
-var token = "29ba1603f1abdb6eacfabacffe49d96d";
+var token = "cea43c435331f22efaa268f4773df21e";
 var city = "San Diego"
 var elName = document.querySelector("#cityName");
 var elTemp = document.querySelector("#temp");
-var elWind = document.querySelector("#wind")
-var elHumidity = document.querySelector("#humidity")
+var elWind = document.querySelector("#wind");
+var elHumidity = document.querySelector("#humidity");
+var elIcon = document.querySelector(".icon");
+
 function createUrl(city) {
     return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + token + "&units=imperial";
 }
@@ -19,13 +21,54 @@ function weatherFetch(city) {
                 elName.textContent = data?.name;
                 elTemp.textContent = data?.main.temp;
                 elWind.textContent = data?.wind.speed;
-                elHumidity
+                var elImg  = document.createElement("img");
+                elImg.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
+                console.log(elImg);
+                elIcon.appendChild(elImg);
+
+                var lat = data.coord.lat;
+                var lon = data.coord.lon;
+
+                var urlc2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&&appid=" + token + "&units=imperial";
+
+                fetch(urlc2).then(function (response) {
+                    if (response.ok) {
+                        response.json().then(function (data) {
+                            console.log(data);
+                            var elForeDiv = document.querySelector(".row");
+                            elForeDiv.innerHTML = `
+                            <div class="col">
+                                <div class="card">
+                                    <ul>
+                                        <li>
+                                            Temp: <span id="temp"></span> F.
+                                        </li>
+                                        <li>
+                                            Wind: <span id="wind"></span> MPH.
+                                        </li>
+                                        <li>
+                                            Humidity: <span id="humidity"></span> %.
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            
+                            `;
+
+
+                        })
+                    }
+                })
+
+
                 
             })
         }
         
     })
  }
+
+
 
 function submitHandler(event) {
     event.preventDefault()
